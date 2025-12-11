48715169a22874a906bc0ab7fcc6968fdd81d250
crox-07 <croxminicat@gmail.com>
2025-12-11 21:23:51 +0000

    Remove API Key
    Ooops.

--- src/utils/YelpAPI.js
+++ src/utils/YelpAPI.js
@@ -1,4 +1,4 @@
-const APIkey = 'nHgsjV_O_RpN5s1zh7Ll47WLOxaCyLNtg0jyht9_IE27X5NGdfQPRoBRnUmL5kX8HrwTrpOH0psVX53qwRmIqiS2-kqsJOfHbursKGT_lrKVY42O8rMETCqg0bI6aXYx';
+const APIkey = import.meta.env.VITE_YELP_API_KEY
 const baseURL = 'https://api.yelp.com/v3';
 
 const getBusiness = async (searchTerms, location, sort_by) => {
